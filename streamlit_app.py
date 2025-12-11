9b7278da5a14465fb6924bb6eba526790d61ad3d
ichua1 <148450551+ichua1@users.noreply.github.com>
2025-12-11 20:08:24 +0000

    Update streamlit_app.py
    from snowflake.snowpark.context import get_active_session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title("🥤 Customize Your Smoothie 🥤")
